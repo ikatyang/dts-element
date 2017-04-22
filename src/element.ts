5625460f79923623daf6a9cfa7660034d8aa5189
ikatyang <ikatyang@gmail.com>
2017-04-22 19:38:55 +0800

    Update element fix clone

--- src/element.ts
+++ src/element.ts
@@ -13,42 +13,48 @@ export abstract class Element<Parameters extends {}> {
   public set(values: Partial<Parameters>): this {
     Object.keys(values).forEach((key: keyof Parameters) => {
       const value = values[key];
-      if (value !== undefined) {
-        this.parameters[key] = value;
-      }
+      this.parameters[key] = value;
     });
     return this;
   }
 
   public clone(is_deep_clone: boolean = false): this {
-    const element = Object.create(this.constructor.prototype);
-    element.parameters = {};
+    const element: this = Object.create(this.constructor.prototype);
     element.copy(this, is_deep_clone);
     return element;
   }
 
   public copy(element: this, is_deep_copy: boolean = false): this {
-    this.parameters = this._clone_parameters(element.parameters, is_deep_copy);
+    this.parameters = this._clone_object(element.parameters, is_deep_copy);
     return this;
   }
 
-  private _clone_parameters(parameters: Parameters, is_deep_clone: boolean): Parameters {
-    return Object.keys(parameters).reduce((cloned_parameters: Partial<Parameters>, key: keyof Parameters) => {
-      const value = parameters[key];
-      const cloned_value = this._clone_value(value, is_deep_clone);
-      cloned_parameters[key] = cloned_value;
-      return cloned_parameters;
-    }, {} as any);
-  }
-
-  private _clone_value<T>(value: T, is_deep_clone: boolean): T {
+  private _clone<T>(value: T, is_deep_clone: boolean): T {
     return (typeof value !== 'object')
       ? value
       : (value instanceof Element)
         ? value.clone(is_deep_clone)
         : (value instanceof Array)
-          ? value.map(<U>(x: U): U => this._clone_value(x, is_deep_clone)) as any
-          : value; // TODO
+          ? this._clone_array(value, is_deep_clone)
+          : this._clone_object(value, is_deep_clone);
+  }
+
+  private _clone_array<T extends any[]>(array: T, is_deep_clone: boolean): T {
+    return array.map(<U>(value: U): U => this._clone_sub_value(value, is_deep_clone)) as T;
+  }
+
+  private _clone_object<T extends {}>(object: T, is_deep_clone: boolean): T {
+    return Object.keys(object).reduce((cloned_object: T, key: keyof T) => {
+      const value = object[key];
+      cloned_object[key] = this._clone_sub_value(value, is_deep_clone);
+      return cloned_object;
+    }, {} as T);
+  }
+
+  private _clone_sub_value<T>(sub_value: T, is_deep_clone: boolean): T {
+    return !is_deep_clone
+      ? sub_value
+      : this._clone(sub_value, is_deep_clone);
   }
 
 }
