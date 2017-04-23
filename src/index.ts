724d69e0df27a83eb14eae3effd5a2c3f1d3ea30
ikatyang <ikatyang@gmail.com>
2017-04-23 23:07:50 +0800

    Update index add document

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export * from './declarations/namespace';
 export * from './types/basic';
 export * from './declaration';
+export * from './document';
 export * from './element';
 export * from './reference';
 export * from './sets';
