280e4c2e41e7f312f308a2671d532a2b73f382da
ikatyang <ikatyang@gmail.com>
2017-04-27 11:56:44 +0800

    Update tests

--- src/helpers/__tests__/line-map.ts
+++ src/helpers/__tests__/line-map.ts
@@ -1,11 +1,18 @@
 import {line_map} from '../line-map';
 
-const fn = (line: string): string => `- ${line} -`;
+const prefix = '- ';
+const postfix = ' -';
+
+const fn = (line: string): string => `${prefix}${line}${postfix}`;
 
 it('should return empty string while input string is empty', () => {
   expect(line_map('', fn)).toBe('');
 });
 
 it('should return mapped string while input string is non-empty', () => {
-  expect(line_map('line 1\nline 2\nline 3', fn)).toBe('- line 1 -\n- line 2 -\n- line 3 -');
+  const line1 = 'line 1';
+  const line2 = 'line 2';
+  const line3 = 'line 3';
+  expect(line_map(`${line1}\n${line2}\n${line3}`, fn))
+    .toBe(`${prefix}${line1}${postfix}\n${prefix}${line2}${postfix}\n${prefix}${line3}${postfix}`);
 });
--- src/helpers/__tests__/trim-every-line.ts
+++ src/helpers/__tests__/trim-every-line.ts
@@ -1,5 +1,8 @@
 import {trim_every_line} from '../trim-every-line';
 
 it('should remove leading spaces and trailing spaces', () => {
-  expect(trim_every_line('  line 1  \n  line 2  \n  line 3  ')).toBe('line 1\nline 2\nline 3');
+  const line1 = 'line 1';
+  const line2 = 'line 2';
+  const line3 = 'line 3';
+  expect(trim_every_line(` ${line1} \n ${line2} \n ${line3} `)).toBe(`${line1}\n${line2}\n${line3}`);
 });
