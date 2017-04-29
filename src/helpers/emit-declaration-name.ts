b7bb3dd6fa4fcaf6c9c933bf47553f11674d8d83
ikatyang <ikatyang@gmail.com>
2017-04-30 00:09:18 +0800

    Update helpers/emit-declaration-name special property should be quoted

--- src/helpers/emit-declaration-name.ts
+++ src/helpers/emit-declaration-name.ts
@@ -2,11 +2,12 @@ import {Container} from '../collections';
 import {Member} from '../elements/member';
 
 const valid_name_regex = /^[\$a-z_][\$a-z_0-9]*$/i;
+const invalid_name_regex = /^(?:new|constructor)$/;
 
 // TODO: add support for Enum
 export const emit_declaration_name = (name: string | null, container: Container): string =>
   (name === null)
     ? ''
-    : !(container instanceof Member) || valid_name_regex.test(name)
+    : !(container instanceof Member) || (valid_name_regex.test(name) && !invalid_name_regex.test(name))
       ? name
       : JSON.stringify(name);
