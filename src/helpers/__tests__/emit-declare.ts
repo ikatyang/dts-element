b534e30136e9c67c0f536e869db9fda046afbe92
ikatyang <ikatyang@gmail.com>
2017-05-02 22:56:50 +0800

    Fix emit-declare fix stack path

--- src/helpers/__tests__/emit-declare.ts
+++ src/helpers/__tests__/emit-declare.ts
@@ -8,10 +8,10 @@ import {emit_declare} from '../emit-declare';
 const element: Element = Object.create(Element.prototype);
 const document: Document = Object.create(Document.prototype);
 
-it('should return empty string while stack is not Document or null', () => {
-  expect(emit_declare(new Stack([element]))).toBe('');
+it('should return empty string while last-stack is not Element-Document', () => {
+  expect(emit_declare(new Stack([element, element]))).toBe('');
 });
 
-it('should return empty string while stack is Document or null', () => {
-  expect(emit_declare(new Stack([document]))).toBe('declare ');
+it('should return empty string while last-stack is Element-Document', () => {
+  expect(emit_declare(new Stack([document, element]))).toBe('declare ');
 });
--- src/helpers/emit-declare.ts
+++ src/helpers/emit-declare.ts
@@ -1,7 +1,8 @@
+import {Element} from '../element';
 import {Document} from '../elements/document';
 import {Stack} from '../stack';
 
 export const emit_declare = (stack: Stack): string =>
-  stack.last_instances_of([Document])
+  stack.last_instances_of([Element as any, Document])
     ? 'declare '
     : '';
