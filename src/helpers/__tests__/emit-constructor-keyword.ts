e198fcc25d947af61f2feda2c5efdd661327970f
ikatyang <ikatyang@gmail.com>
2017-05-02 23:25:01 +0800

    Fix emit-constructor-keyword/return fix stack path

--- src/helpers/__tests__/emit-constructor-keyword.ts
+++ src/helpers/__tests__/emit-constructor-keyword.ts
@@ -1,15 +1,17 @@
 jest.unmock('../emit-constructor-keyword');
 
+import {Element} from '../../element';
 import {ClassMember} from '../../elements/members/class-member';
 import {Stack} from '../../stack';
 import {emit_constructor_keyword} from '../emit-constructor-keyword';
 
+const element: Element = Object.create(Element.prototype);
 const class_member: ClassMember = Object.create(ClassMember.prototype);
 
-it('should return "constructor" while stack is ClassMember', () => {
-  expect(emit_constructor_keyword(new Stack([class_member]))).toBe('constructor');
+it('should return "constructor" while last stack is Element-ClassMember', () => {
+  expect(emit_constructor_keyword(new Stack([class_member, element]))).toBe('constructor');
 });
 
-it('should return "new" while stack is not ClassMember', () => {
+it('should return "new" while last stack is not Element-ClassMember', () => {
   expect(emit_constructor_keyword(new Stack())).toBe('new');
 });
--- src/helpers/emit-constructor-return.ts
+++ src/helpers/emit-constructor-return.ts
@@ -1,8 +1,9 @@
+import {Element} from '../element';
 import {ClassMember} from '../elements/members/class-member';
 import {Type} from '../elements/type';
 import {Stack} from '../stack';
 
 export const emit_constructor_return = (return_type: Type, stack: Stack): string =>
-  (stack.last_instances_of([ClassMember]))
+  (stack.last_instances_of([Element as any, ClassMember]))
     ? ''
     : `: ${return_type.emit(stack)}`;
