fad8adbc1da709b71232729b35442cd866e2144d
ikatyang <ikatyang@gmail.com>
2017-05-03 10:15:16 +0800

    Fix emit-export fix stack path

--- src/helpers/emit-export.ts
+++ src/helpers/emit-export.ts
@@ -1,8 +1,13 @@
 import {Element} from '../element';
+import {ModuleDeclaration} from '../elements/declarations/module-declaration';
 import {Document} from '../elements/document';
 import {Stack} from '../stack';
 
+const is_exportable_stack = (stack: Stack): boolean =>
+  stack.last_instances_of([Element as any, Document]) ||
+  stack.last_instances_of([Element as any, ModuleDeclaration]);
+
 export const emit_export = (has_export: boolean, stack: Stack): string =>
-  (has_export && stack.last_instances_of([Element as any, Document]))
+  (has_export && is_exportable_stack(stack))
     ? 'export '
     : '';
