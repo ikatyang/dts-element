cb9d46462278597b4e70de97e2aed007a70db113
ikatyang <ikatyang@gmail.com>
2017-05-04 11:26:51 +0800

    Update tests use emit() instead of _emit()

--- src/elements/members/__tests__/enum-member.ts
+++ src/elements/members/__tests__/enum-member.ts
@@ -4,11 +4,11 @@ import {Stack} from '../../../stack';
 import {VariableDeclaration} from '../../declarations/variable-declaration';
 import {EnumMember} from '../enum-member';
 
-describe('#_emit()', () => {
+describe('#emit()', () => {
   it('should return correctly', () => {
     const fn = (): number => 1;
     expect(new EnumMember({
       owned: new VariableDeclaration({name: 'x'}),
-    })._emit(new Stack(), fn)).toMatchSnapshot();
+    }).emit(new Stack(), fn)).toMatchSnapshot();
   });
 });
