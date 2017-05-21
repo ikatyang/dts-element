6862f07483041f254b7a5099dfaeae13fb474a04
ikatyang <ikatyang@gmail.com>
2017-05-21 18:00:34 +0800

    Add parse/keyword-types

--- src/__tests__/parse.ts
+++ src/__tests__/parse.ts
@@ -5,9 +5,22 @@ import {parse, parse_native} from '../parse';
 const code = `
 /// <reference path="some-path" />
 /// <reference types="some-types" />
-declare var variable_declaration_var;
-declare let variable_declaration_let;
-declare const variable_declaration_const;
+
+declare var v_var;
+declare let v_let;
+declare const v_const;
+
+declare const v_any: any;
+declare const v_boolean: boolean;
+declare const v_never: never;
+declare const v_null: null;
+declare const v_number: number;
+declare const v_object: object;
+declare const v_string: string;
+declare const v_symbol: symbol;
+declare const v_this: this;
+declare const v_undefined: undefined;
+declare const v_void: void;
 `;
 
 it('should return correctly', () => {
--- src/parse.ts
+++ src/parse.ts
@@ -1,5 +1,17 @@
 import * as ts from 'typescript';
-import {any_type} from './constants';
+import {
+  any_type,
+  boolean_type,
+  never_type,
+  null_type,
+  number_type,
+  object_type,
+  string_type,
+  symbol_type,
+  this_type,
+  undefined_type,
+  void_type,
+} from './constants';
 import {IElement} from './element';
 import {ITopLevelElement} from './others/top-level-element';
 import {parse_source_file} from './parsers/source-file';
@@ -8,8 +20,18 @@ import {parse_variable_statement} from './parsers/variable-statement';
 export const parse_native = (node: ts.Node): IElement<any> => {
   switch (node.kind) {
     case ts.SyntaxKind.AnyKeyword: return any_type;
+    case ts.SyntaxKind.BooleanKeyword: return boolean_type;
+    case ts.SyntaxKind.NeverKeyword: return never_type;
+    case ts.SyntaxKind.NullKeyword: return null_type;
+    case ts.SyntaxKind.NumberKeyword: return number_type;
+    case ts.SyntaxKind.ObjectKeyword: return object_type;
     case ts.SyntaxKind.SourceFile: return parse_source_file(node as ts.SourceFile);
+    case ts.SyntaxKind.StringKeyword: return string_type;
+    case ts.SyntaxKind.SymbolKeyword: return symbol_type;
+    case ts.SyntaxKind.ThisType: return this_type;
+    case ts.SyntaxKind.UndefinedKeyword: return undefined_type;
     case ts.SyntaxKind.VariableStatement: return parse_variable_statement(node as ts.VariableStatement);
+    case ts.SyntaxKind.VoidKeyword: return void_type;
     default:
       throw new Error(`Unexpected ts-kind ${node.kind} ( ${ts.SyntaxKind[node.kind]} )`);
   }
