5e85fb3a60b374fd666457d6af132fd766af3d32
ikatyang <ikatyang@gmail.com>
2017-08-12 18:26:59 +0800

    fix(sub-type): should transform correctly

--- src/parsers/indexed-access-type.ts
+++ src/parsers/indexed-access-type.ts
@@ -4,13 +4,8 @@ import { create_sub_type, ISubType } from '../types/sub-type';
 
 export const parse_indexed_access_type = (
   node: ts.IndexedAccessTypeNode,
-): ISubType => {
-  const types = [node.objectType, node.indexType];
-  while (types[types.length - 1].kind === ts.SyntaxKind.IndexedAccessType) {
-    const last_type = types[types.length - 1] as ts.IndexedAccessTypeNode;
-    types.push(last_type.indexType);
-  }
-  return create_sub_type({
-    types: types.map(parse_native),
+): ISubType =>
+  create_sub_type({
+    object: parse_native(node.objectType),
+    index: parse_native(node.indexType),
   });
-};
